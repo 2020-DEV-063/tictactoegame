cb36c589f9300446bccfa3f42ce80f9792ddc223
Gdevlees <glen.devleeschouwer@hotmail.com>
2020-04-03 21:31:33 +0200

    Added PositionOccupiedException in exceptions package

---
+++ src/main/java/com/game/tictactoegame/exceptions/PositionOccupiedException.java
@@ -0,0 +1,14 @@
+package com.game.tictactoegame.exceptions;
+
+/**
+ * This class holds the PositionOccupiedException
+ * @Author 2020-DEV-063
+ */
+public class PositionOccupiedException extends RuntimeException{
+    String message = "This position is already occupied. Please, choose another position to put you marker.";
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
