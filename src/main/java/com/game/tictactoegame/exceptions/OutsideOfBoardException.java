38cf344068a6dc7012e833c71a2ebf83df1903a1
Gdevlees <glen.devleeschouwer@hotmail.com>
2020-04-03 23:21:20 +0200

    Refactored OutsideOfBoardException class

--- src/main/java/com/game/tictactoegame/exceptions/OutsideOfBoardException.java
+++ src/main/java/com/game/tictactoegame/exceptions/OutsideOfBoardException.java
@@ -1,14 +1,19 @@
 package com.game.tictactoegame.exceptions;
 
+import com.game.tictactoegame.util.Constants;
+
+import static com.game.tictactoegame.util.Constants.OUTSIDEOFBOARD_EXCEPTION_MSG;
+
 /**
  * This class holds the OutsideOfBoardException
  * @Author 2020-DEV-063
  */
 public class OutsideOfBoardException extends RuntimeException {
-    private String message = "This position is outside of the board. Please, choose another position to put you marker.";
+
+    private static final long serialVersionUID = 1L;
 
     @Override
     public String getMessage() {
-        return message;
+        return OUTSIDEOFBOARD_EXCEPTION_MSG;
     }
 }
