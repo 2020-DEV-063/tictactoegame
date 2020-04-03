9fb5a8aacdd7ed24b9e210692a04a7f478eb4229
Gdevlees <glen.devleeschouwer@hotmail.com>
2020-04-03 16:44:37 +0200

    Added testclass for the TicTacToeGame class added test method to check the number of positions on the board

---
+++ src/test/java/com/game/tictactoegame/test/TicTacToeGameTest.java
@@ -0,0 +1,29 @@
+package com.game.tictactoegame.test;
+
+import com.game.tictactoegame.service.TicTacToeGame;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * This class is to test the TicTacToeGame class
+ */
+class TicTacToeGameTest {
+
+    private TicTacToeGame ticTacToeGame;
+
+    @BeforeEach
+    void init() {
+        ticTacToeGame = new TicTacToeGame();
+    }
+
+    /**
+     * A test to check if there are 9 empty positions on the board
+     */
+    @Test
+    void testNumberOfPositionsOnBoard() {
+        String[][] testBoard = ticTacToeGame.getBoard();
+        assertEquals(9, testBoard[0].length + testBoard[1].length + testBoard[2].length);
+    }
+}
