8d33a829081f7bf2e1839173b808206c84bdfa4d
Gdevlees <glen.devleeschouwer@hotmail.com>
2020-04-03 17:16:47 +0200

    Added Player pojo class

---
+++ src/main/java/com/game/tictactoegame/pojo/Player.java
@@ -0,0 +1,17 @@
+package com.game.tictactoegame.pojo;
+
+/**
+ * This class holds the Player pojo
+ * @Author 2020-DEV-063
+ */
+public class Player {
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
