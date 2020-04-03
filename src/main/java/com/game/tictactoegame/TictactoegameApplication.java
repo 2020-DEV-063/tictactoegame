05026f921c4edb2c6d16161777e8c2d7cbd7e724
Gdevlees <glen.devleeschouwer@hotmail.com>
2020-04-03 23:55:58 +0200

    Updated main class to get a bean of ticTacToeGame and call newGame method

--- src/main/java/com/game/tictactoegame/TictactoegameApplication.java
+++ src/main/java/com/game/tictactoegame/TictactoegameApplication.java
@@ -3,14 +3,15 @@ package com.game.tictactoegame;
 import com.game.tictactoegame.service.TicTacToeGame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class TictactoegameApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TictactoegameApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(TictactoegameApplication.class, args);
 
-        TicTacToeGame ticTacToeGame = new TicTacToeGame();
+        TicTacToeGame ticTacToeGame = (TicTacToeGame) ctx.getBean("ticTacToeGame");
         ticTacToeGame.newGame();
     }
 
