35a911170f05062c6ff3348566d1eea8938579e2
Gdevlees <glen.devleeschouwer@hotmail.com>
2020-04-03 21:08:56 +0200

    Added marker variable to the Player class

--- src/main/java/com/game/tictactoegame/pojo/Player.java
+++ src/main/java/com/game/tictactoegame/pojo/Player.java
@@ -6,12 +6,18 @@ package com.game.tictactoegame.pojo;
  */
 public class Player {
     private String name;
+    private String marker;
 
-    public Player(String name) {
+    public Player(String name, String marker) {
         this.name = name;
+        this.marker = marker;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getMarker() {
+        return marker;
     }
 }
