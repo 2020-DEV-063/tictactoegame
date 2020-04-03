104f7c0e9df2eb768f7e7f4e2aa97e502c40224c
Gdevlees <glen.devleeschouwer@hotmail.com>
2020-04-03 17:17:51 +0200

    Added player1 and player2 name Constants Added constant for the turnCounter at the beginning of the game

--- src/main/java/com/game/tictactoegame/util/Constants.java
+++ src/main/java/com/game/tictactoegame/util/Constants.java
@@ -8,4 +8,7 @@ public class Constants {
     public static final String[][] EMPTY_BOARD = {{" - ", " - ", " - "}, {" - ", " - ", " - "}, {" - ", " - ", " - "}};
     public static final String LAYOUT_SEPARATOR = " | ";
     public static final String NEW_LINE = "\n";
+    public static final String PLAYER1_NAME = "Player 1";
+    public static final String PLAYER2_NAME = "Player 2";
+    public static final int ZERO_TURNS_PLAYED = 0;
 }
