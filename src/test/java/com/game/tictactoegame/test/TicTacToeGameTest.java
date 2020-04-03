240e316ccd3e8e2138accc8f11a593250dffaffb
Gdevlees <glen.devleeschouwer@hotmail.com>
2020-04-03 17:04:24 +0200

    added test to check if the boardLayout is not empty after using the printBoard method

--- src/test/java/com/game/tictactoegame/test/TicTacToeGameTest.java
+++ src/test/java/com/game/tictactoegame/test/TicTacToeGameTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 /**
  * This class is to test the TicTacToeGame class
@@ -19,11 +20,22 @@ class TicTacToeGameTest {
     }
 
     /**
-     * A test to check if there are 9 empty positions on the board
+     * To check if there are 9 empty positions on the board
      */
     @Test
     void testNumberOfPositionsOnBoard() {
         String[][] testBoard = ticTacToeGame.getBoard();
         assertEquals(9, testBoard[0].length + testBoard[1].length + testBoard[2].length);
+    }
+
+    /**
+     * To check if the boardLayout is empty or not
+     */
+    @Test
+    void testBoardLayoutIsCreated(){
+        TicTacToeGame ticTacToeGame = new TicTacToeGame();
+        ticTacToeGame.printBoard();
+
+        assertFalse(ticTacToeGame.getBoardLayout().toString().isEmpty());
     }
 }
