ca82d0c89f6d14ba00713dda7d667c5ccab711ad
Gdevlees <glen.devleeschouwer@hotmail.com>
2020-04-03 16:40:18 +0200

    Added Constants class under util package to maintain the constants

---
+++ src/main/java/com/game/tictactoegame/util/Constants.java
@@ -0,0 +1,9 @@
+package com.game.tictactoegame.util;
+
+/**
+ * This class holds all the constants
+ * @Author 2020-DEV-063
+ */
+public class Constants {
+    public static final String[][] EMPTY_BOARD = {{" - ", " - ", " - "}, {" - ", " - ", " - "}, {" - ", " - ", " - "}};
+}
