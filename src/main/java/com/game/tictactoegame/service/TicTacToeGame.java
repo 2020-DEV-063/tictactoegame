f2403ddbd8002ffd0be274044075d1f3d5d47282
Gdevlees <glen.devleeschouwer@hotmail.com>
2020-04-03 17:19:45 +0200

    Added player1 and player2 variable Added turnCounter and playerAtTurn variable Added a method to determine who's turn it is

--- src/main/java/com/game/tictactoegame/service/TicTacToeGame.java
+++ src/main/java/com/game/tictactoegame/service/TicTacToeGame.java
@@ -1,5 +1,7 @@
 package com.game.tictactoegame.service;
 
+import com.game.tictactoegame.pojo.Player;
+
 import static com.game.tictactoegame.util.Constants.*;
 
 /**
@@ -10,6 +12,22 @@ public class TicTacToeGame {
 
     private String [][] board = EMPTY_BOARD;
     private StringBuilder boardLayout;
+
+    private Player player1 = new Player(PLAYER1_NAME);
+    private Player player2 = new Player(PLAYER2_NAME);
+    private int turnCounter = ZERO_TURNS_PLAYED;
+    private Player playerAtTurn;
+
+    /**
+     * This method uses the turnCounter to determine who's turn it is
+     */
+    public void determineWhoIsAtTurn(){
+        if(turnCounter % 2 == 0){
+            playerAtTurn = player1;
+        } else {
+            playerAtTurn = player2;
+        }
+    }
 
     /**
      * This method will update the boardLayout and print it to the console
@@ -31,6 +49,22 @@ public class TicTacToeGame {
 
     public StringBuilder getBoardLayout() {
         return boardLayout;
+    }
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+
+    public Player getPlayerAtTurn() {
+        return playerAtTurn;
+    }
+
+    public void setTurnCounter(int turnCounter) {
+        this.turnCounter = turnCounter;
     }
 
 }
