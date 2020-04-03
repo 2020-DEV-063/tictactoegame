9b13e1d37e108a7c5d009bf32e7623f3377329ff
Gdevlees <glen.devleeschouwer@hotmail.com>
2020-04-03 17:02:54 +0200

    Added method to print the tictactoe board to the console

--- src/main/java/com/game/tictactoegame/service/TicTacToeGame.java
+++ src/main/java/com/game/tictactoegame/service/TicTacToeGame.java
@@ -1,6 +1,6 @@
 package com.game.tictactoegame.service;
 
-import static com.game.tictactoegame.util.Constants.EMPTY_BOARD;
+import static com.game.tictactoegame.util.Constants.*;
 
 /**
  * This class holds the game logic
@@ -9,9 +9,28 @@ import static com.game.tictactoegame.util.Constants.EMPTY_BOARD;
 public class TicTacToeGame {
 
     private String [][] board = EMPTY_BOARD;
+    private StringBuilder boardLayout;
+
+    /**
+     * This method will update the boardLayout and print it to the console
+     */
+    public void printBoard(){
+        boardLayout = new StringBuilder();
+        for(int row =0; row < 3; row ++) {
+            for(int column=0; column < 3 ; column ++ ) {
+                boardLayout.append(LAYOUT_SEPARATOR).append(board[row][column]);
+            }
+            boardLayout.append(LAYOUT_SEPARATOR).append(NEW_LINE);
+        }
+        System.out.println(boardLayout.toString());
+    }
 
     public String[][] getBoard() {
         return board;
+    }
+
+    public StringBuilder getBoardLayout() {
+        return boardLayout;
     }
 
 }
