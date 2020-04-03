19364bb5f849b9d2baa9a5df735c34e3a8c454bb
Gdevlees <glen.devleeschouwer@hotmail.com>
2020-04-03 22:09:53 +0200

    Updated String variable message to be private

--- src/main/java/com/game/tictactoegame/exceptions/PositionOccupiedException.java
+++ src/main/java/com/game/tictactoegame/exceptions/PositionOccupiedException.java
@@ -5,7 +5,7 @@ package com.game.tictactoegame.exceptions;
  * @Author 2020-DEV-063
  */
 public class PositionOccupiedException extends RuntimeException{
-    String message = "This position is already occupied. Please, choose another position to put you marker.";
+    private String message = "This position is already occupied. Please, choose another position to put you marker.";
 
     @Override
     public String getMessage() {
